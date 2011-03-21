78cd690c1f94e2d85b77bab60dc00ffff224cbdb
Bernhard Häussner <bxt@die-optimisten.net>
2011-03-22 00:17:31 +0100

    Extended benchmarks

--- php/Mustaml/Cli.php
+++ php/Mustaml/Cli.php
@@ -7,6 +7,9 @@ class Cli {
 		if(isset($argv[0])&&isset($argv[1])) {
 			$data=json_decode(file_get_contents($argv[0]),true);
 			$templateString=file_get_contents($argv[1]);
+			
+			// Bench:
+			/*
 			$pr=new \Profile('bench.csv');
 			for($i=0;$i<10000;$i++) {
 				$p=new Parser();
@@ -19,6 +22,17 @@ class Cli {
 				$c->render($ast,$data);
 			}
 			$pr->end("htmlclass x $i");
+			*/
+			
+			// Normal run:
+			
+			$p=new Parser();
+			$ast=$p->parseString($templateString);
+			$c=new HtmlCompiler();
+			$c->render($ast,$data)."\n";
+			
+			
+			// Test:
 			//echo md5($c->render($ast,$data))."\n";
 			//var_dump($c->render($ast,$data));
 		}
