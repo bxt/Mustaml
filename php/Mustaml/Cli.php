aa951c21f639c66dc98729e5f9d726a305b30c9e
Bernhard Häussner <bxt@die-optimisten.net>
2011-03-22 00:18:44 +0100

    Removed Benchmarks

--- php/Mustaml/Cli.php
+++ php/Mustaml/Cli.php
@@ -8,33 +8,12 @@ class Cli {
 			$data=json_decode(file_get_contents($argv[0]),true);
 			$templateString=file_get_contents($argv[1]);
 			
-			// Bench:
-			/*
-			$pr=new \Profile('bench.csv');
-			for($i=0;$i<10000;$i++) {
-				$p=new Parser();
-				$ast=$p->parseString($templateString);
-			}
-			$pr->end("parse4htmlclass x $i");
-			$pr=new \Profile('bench.csv');
-			for($i=0;$i<10000;$i++) {
-				$c=new HtmlCompiler();
-				$c->render($ast,$data);
-			}
-			$pr->end("htmlclass x $i");
-			*/
-			
-			// Normal run:
-			
 			$p=new Parser();
 			$ast=$p->parseString($templateString);
 			$c=new HtmlCompiler();
-			$c->render($ast,$data)."\n";
+			$html=$c->render($ast,$data)."\n";
 			
-			
-			// Test:
-			//echo md5($c->render($ast,$data))."\n";
-			//var_dump($c->render($ast,$data));
+			var_dump($html);
 		}
 	}
 }
