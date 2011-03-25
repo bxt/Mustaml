c81312a9a39e3c591156351cb84a4856d669a8fb
Bernhard Häussner <bxt@die-optimisten.net>
2011-03-25 20:42:05 +0100

    Added ability to load .json fiels along with templates

--- php/Mustaml/Autoloaders/TemplateDirAl.php
+++ php/Mustaml/Autoloaders/TemplateDirAl.php
@@ -9,13 +9,21 @@ class TemplateDirAl implements AutoloaderI {
 		$this->mustamlBoilerplate=$mustamlBoilerplate ?: new \Mustaml\Mustaml('');
 	}
 	public function autoload($key) {
-		if(preg_match('/\.mustaml$/i',$key)) {
+		if(preg_match('/^(.*)\.mustaml$/i',$key,$m)) {
 			// only for vars like *.mustaml to avoid loading random stuff
+			
+			$data=array();
+			$possibleJSONString=@file_get_contents($this->templateDir.'/'.$m[1].'.json');
+			if($possibleJSONString) {
+				$jsonData=json_decode($possibleJSONString,true);
+				if($data!==null) $data=$jsonData;
+			}
+			
 			$possibleTemplateString=@file_get_contents($this->templateDir.'/'.$key);
 			if(!$possibleTemplateString) {
 				return null;
 			} else {
-				$v=$this->mustamlBoilerplate->getWithTemplate($possibleTemplateString);
+				$v=$this->mustamlBoilerplate->getWithTemplate($possibleTemplateString,$data);
 				return $v;
 			}
 		}
