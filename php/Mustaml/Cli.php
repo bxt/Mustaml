970726d4677bfa584f089b674659f8adf2221309
Bernhard Häussner <bxt@die-optimisten.net>
2011-03-25 02:24:10 +0100

    Added template include capabilities to CLI script and made .json file parameter optional

--- php/Mustaml/Cli.php
+++ php/Mustaml/Cli.php
@@ -4,18 +4,31 @@ namespace Mustaml;
 class Cli {
 	static function run($argv) {
 		$script=array_shift($argv);
+		if(!isset($argv[0])) {
+			echo 'Need at least 1 Parameter'."\n";
+			return 0;
+		}
 		if(isset($argv[0])&&isset($argv[1])) {
+			$templateString=file_get_contents($argv[1]);
 			$data=json_decode(file_get_contents($argv[0]),true);
 			if($data===null) throw new \Exception("Invalid JSON!");
-			$templateString=file_get_contents($argv[1]);
-			
-			$p=new Parser();
-			$ast=$p->parseString($templateString);
-			//var_dump($ast);
-			$c=new HtmlCompiler();
-			$html=$c->render($ast,$data)."\n";
-			
-			echo $html;
+		} else {
+			$templateString=file_get_contents($argv[0]);
+			$data=array();
 		}
+		
+		$al=new Autoloaders\TemplateDirAutoloader('.'); //pwd
+		$config=new HtmlCompilerConfig(array(array($al,'autoload')));
+		$al_bp=new Mustaml('',array(),$config);
+		$al->setMustamlBoilerplate($al_bp);
+		
+		$p=new Parser();
+		$ast=$p->parseString($templateString);
+		//var_dump($ast);
+		$c=new HtmlCompiler($config);
+		$html=$c->render($ast,$data)."\n";
+		
+		echo $html;
+		return 1;
 	}
 }
