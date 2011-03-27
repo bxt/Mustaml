c6b4f76f6e156c550128ef606ff2aa5b200b7cc1
Bernhard Häussner <bxt@die-optimisten.net>
2011-03-28 00:07:10 +0200

    Added Tests for TemplateDirAl

--- test-php/Mustaml/Autoloaders/TemplateDirAlTest.php
+++ test-php/Mustaml/Autoloaders/TemplateDirAlTest.php
@@ -21,5 +21,23 @@ class TemplateDirAlTest extends \PHPUnit_Framework_TestCase {
 		
 		$this->assertEquals('<html><div id="hello">from ahorn.mustaml with love<div id="hello">from banshee.mustaml<p class="bansheeblock"><span id="enbloc"></span></p></div></div></html>',$main());
 	}
+	public function testNotFindingTemplateFile() {
+		
+		$data=array("love"=>"with love");
+		$config=new \Mustaml\Html\CompilerConfig();
+		$config->registerAutoloader(new TemplateDirAl('test-php/data'));
+		$main=new \Mustaml\Mustaml("-nonexistent.mustaml",$data,$config);
+		
+		$this->assertEquals('',$main());
+	}
+	public function testLoadingInvalidJSONData() {
+		
+		$data=array("love"=>"with love");
+		$config=new \Mustaml\Html\CompilerConfig();
+		$config->registerAutoloader(new TemplateDirAl('test-php/data'));
+		$main=new \Mustaml\Mustaml("-invalid-json.mustaml",$data,$config);
+		
+		$this->assertEquals('<p>invalid json is silenty ignored</p>',$main());
+	}
 }
 ?>
