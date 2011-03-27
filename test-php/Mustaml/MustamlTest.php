ba833558a01e563ec9662ac5f7beaf137bfb90ab
Bernhard Häussner <bxt@die-optimisten.net>
2011-03-27 18:04:44 +0200

    Testing our new iterative power

--- test-php/Mustaml/MustamlTest.php
+++ test-php/Mustaml/MustamlTest.php
@@ -3,6 +3,16 @@ namespace Mustaml;
 require_once 'mustaml.php';
 
 class MustamlTest extends \PHPUnit_Framework_TestCase {
+	/**
+	 * this is the maximum nesing level
+	 * we souldn't have too much revursion here
+	 * and PHP souldn't run out of adresses etc.
+	 * and don't set it too high, because then
+	 * tests tun forever and eat all reccources
+	 */
+	const MAX_NESTING_LVL=400;
+	//const MAX_NESTING_LVL=4000;
+	
 	public function testNstingWithoutSubblock() {
 		
 		$child=new Mustaml("#child\n  %p =foo\n  %p =bam",array("foo"=>"override"));
@@ -18,7 +28,7 @@ class MustamlTest extends \PHPUnit_Framework_TestCase {
 		$this->assertEquals('<p><div id="child"><p>override</p><p>inherit</p></div><div id="childblock"><span>bar</span><span>additional</span></div></p><p>bar</p><p>inherit</p>',$main());
 	}
 	public function testMaximumNestingLevel() {
-		$lvl=27;
+		$lvl=self::MAX_NESTING_LVL;
 		
 		$mustaml='';
 		$ws='';
@@ -35,7 +45,7 @@ class MustamlTest extends \PHPUnit_Framework_TestCase {
 		$this->assertEquals($html_a.$html_b,$main());
 	}
 	public function testMaximumNestingLevelOneliner() {
-		$lvl=27;
+		$lvl=self::MAX_NESTING_LVL;
 		
 		$mustaml='';
 		$html_a='';
